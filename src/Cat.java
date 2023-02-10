358979ecef391dd758d883896a3a552ba310e1e7
KIRKA1356 <kgritsun@mail.ru>
2023-02-10 16:17:01 +0300

    v.1.0

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,101 @@
-package PACKAGE_NAME;public class Cat {
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private String owner;
+    private int age;
+    private String breed;
+    private int id;
+    private HashSet<String> vaccinated;
+
+    public Cat(String name, String owner, int age, String breed, int id, HashSet<String> vaccinated) {
+        this.name = name;
+        this.owner = owner;
+        this.age = age;
+        this.breed = breed;
+        this.id = id;
+        this.vaccinated = vaccinated;
+    }
+    public Cat() {
+
+    }
+    public Boolean isVacc(String vacc){
+        if (vaccinated.contains(vacc)) return true;
+        else return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", owner='" + owner + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                ", id=" + id +
+                ", vaccinated=" + vaccinated +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && id == cat.id && Objects.equals(name, cat.name) && Objects.equals(owner, cat.owner) && Objects.equals(breed, cat.breed) && Objects.equals(vaccinated, cat.vaccinated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, owner, age, breed, id, vaccinated);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public HashSet<String> getVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(HashSet<String> vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
-public class Cat {
-    String name;
-    String owner;
-    int age;
-    String breed;
-    int id;
-    String[] vaccinated;
+import java.util.Arrays;
+import java.util.HashSet;
 
+public class Main {
     public static void main(String[] args) {
         Cat tom = new Cat();
-    tom.name = "Tom";
-    tom.age = 5;
-    
-    }
-    public static Boolean isVaccinated(){
+        tom.setName("Tom");
+        tom.setAge(5);
+        tom.setOwner("Kristian Bale");
+        tom.setBreed("Chihir");
+        tom.setId(012);
+        tom.setVaccinated(new HashSet<String>(Arrays.asList("qwe", "tur")));
+        System.out.printf("Name: %s \nAge: %d\nVacc: %s\n", tom.getName(), tom.getAge(), tom.getVaccinated());
+        System.out.println(tom.isVacc("sputnik5"));
+
+        Cat barsik = new Cat();
+        barsik.setName("Barsik");
+        barsik.setAge(2);
+        barsik.setId(022);
+        barsik.setBreed("Tunic");
+        barsik.setVaccinated(new HashSet<String>(Arrays.asList("sputnik69","adres")));
+
+        Cat marta = new Cat("Marta", "Rich Jhons", 3, "Chernya", 34,
+                    new HashSet<String>(Arrays.asList("sad","adadf")));
+
+        System.out.println(barsik.toString());
+        System.out.println(tom);
+        System.out.println(marta);
 
     }
 }
