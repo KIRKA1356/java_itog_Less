d71501d69b060359fa42e82e17f209138951c5cd
KIRKA1356 <kgritsun@mail.ru>
2023-02-10 16:16:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Cat {
+    String name;
+    String owner;
+    int age;
+    String breed;
+    int id;
+    String[] vaccinated;
+
+    public static void main(String[] args) {
+        Cat tom = new Cat();
+    tom.name = "Tom";
+    tom.age = 5;
+    
+    }
+    public static Boolean isVaccinated(){
+
+    }
+}
